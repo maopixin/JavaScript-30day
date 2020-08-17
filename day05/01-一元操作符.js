306bf50afe7cacf4807eb76690833c0a3518808c
oliver <18364076997@163.com>
2020-08-17 14:02:38 +0800

    fix: day05

---
+++ day05/01-一元操作符.js
@@ -0,0 +1,5 @@
+var age = 20;
+++age; 
+--age;
+age++;
+age--;
---
+++ day05/03-布尔操作符.js
@@ -0,0 +1,6 @@
+console.log(!false); // true
+console.log(!'blue'); // fasle
+console.log(!''); // true
+console.log(!0); // true
+console.log(!1); // fasle
+console.log(!NaN); // true
